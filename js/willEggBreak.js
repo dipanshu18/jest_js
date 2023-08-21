3cd72a22c7eacf99d82eb8106993b924ee54f1c4
dipanshu18 <torawanedipanshu@gmail.com>
2023-08-21 19:05:38 +0530

    Testing

--- js/willEggBreak.js
+++ js/willEggBreak.js
@@ -1,15 +1,17 @@
 function inflection(floor) {
-    const inflectionPoint = 47;
-        if (floor >= inflectionPoint) {
-            return true
-        }   
+  const inflectionPoint = 47;
+  if (floor >= inflectionPoint) {
+    return true;
+  }
 }
 
 function isBreak(floorNo) {
-    if (inflection(floorNo)) {
-        return true;
-    }
-    return false;
+  if (inflection(floorNo)) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = isBreak;
+
+// This is an interview question
